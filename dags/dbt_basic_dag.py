0601584758b82a920d47889b97839af3210fe855
aoelvp94 <aoelvp94@gmail.com>
2024-03-25 02:26:50 -0300

    rm restart_data function

--- dags/dbt_basic_dag.py
+++ dags/dbt_basic_dag.py
@@ -4,23 +4,15 @@ A basic dbt DAG that shows how to run dbt commands via the BashOperator
 Follows the standard dbt seed, run, and test pattern.
 """
 import os
-from pathlib import Path
 from pendulum import datetime
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python import PythonOperator
 
 # We're hardcoding this value here for the purpose of the demo, but in a production environment this
 # would probably come from a config file and/or environment variables!
 DBT_PROJECT_DIR = os.environ["DBT_PROJECT_DIR"]
 
-def restart_data():
-    try:
-        file_to_rem = Path(f"{DBT_PROJECT_DIR}/jaffle_shop.duckdb")
-        file_to_rem.unlink()
-    except:
-        pass
 
 with DAG(
     "dbt_basic_dag",
@@ -29,8 +21,6 @@ with DAG(
     schedule=None,
     catchup=False,
 ) as dag:
-
-    restart_data_task = PythonOperator(task_id="restart_data", python_callable=restart_data)
 
     # This task loads the CSV files from dbt/data into the local postgres database for the purpose of this demo.
     # In practice, we'd usually expect the data to have already been loaded to the database.
@@ -49,4 +39,4 @@ with DAG(
         bash_command=f"cd {DBT_PROJECT_DIR} && dbt test --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
     )
 
-    restart_data_task >> dbt_seed >> dbt_run >> dbt_test
+    dbt_seed >> dbt_run >> dbt_test
